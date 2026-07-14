f4a469951b615a8da2f41baeb6489f5a46c22b13
ZhurAlex <265831799+ZhurAlex@users.noreply.github.com>
2026-07-14 14:01:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from config import BOT_TOKEN
+
+
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello! I'm a bot created with aiogram.")
+
+
+async def main() -> None:
+    bot = Bot(token=BOT_TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+          
